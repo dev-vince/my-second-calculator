6aba0c2fa12d94525de6a519f687713129e2b3a2
Matthew Perkins <beandog420@gmail.com>
2022-05-31 21:02:13 -0700

    Hello!

---
+++ Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        { Console.WriteLine("Welcome to the Calculator!");
+            Console.WriteLine("Please enter your first number:"); string firstNumber = Console.ReadLine(); Console.WriteLine("Please enter your second number:"); string secondNumber = Console.ReadLine(); Console.WriteLine("Please enter your operator:"); string operatorInput = Console.ReadLine(); double firstNumberDouble = double.Parse(firstNumber); double secondNumberDouble = double.Parse(secondNumber);
+            double result = 0; switch (operatorInput) { case "+": result = firstNumberDouble + secondNumberDouble; break; case "-": result = firstNumberDouble - secondNumberDouble; break; case "*": result = firstNumberDouble * secondNumberDouble; break; case "/": result = firstNumberDouble / secondNumberDouble; break; default: Console.WriteLine("Invalid operator"); break; } Console.WriteLine($"The result is {result}"); Console.ReadLine();
+        }
+    }
+}
